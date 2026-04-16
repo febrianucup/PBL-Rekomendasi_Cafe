bea55c6104c1cbabed833b738e0cdcd06d7c2069
febrian <muhammaddwifebrian@gmail.com>
2026-04-16 21:03:31 +0700

    detail cafe

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/detail/1', function () {
+    return view('../DetailCafe/detailcafe');
+});
