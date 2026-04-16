666a530e083798b6cf1850b7f47a102351247084
febrian <muhammaddwifebrian@gmail.com>
2026-04-16 22:14:24 +0700

    listcafe guest

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('../ListCafe/listCafe');
 });
 
 Route::get('/detail/1', function () {
