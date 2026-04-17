429c5272ac3be9025a7979a0e02093a27c7eb240
adytriaby <aditriyabayuramadani@gmail.com>
2026-04-17 09:00:19 +0700

    admin

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,21 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return redirect('/admin/cafes');
+    });
+    
+    Route::get('/cafes', function () {
+        return view('admin.cafes');
+    });
+    
+    Route::get('/accounts', function () {
+        return view('admin.accounts');
+    });
+    
+    Route::get('/comments', function () {
+        return view('admin.comments');
+    });
+});
