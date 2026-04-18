a1fb7ef67f0f614904fd61c517287f549a69ba6c
hasbi <muhammadhasbiasidiki175@gmail.com>
2026-04-18 10:41:03 +0700

    perbaiki lupa password dan register
    Signed-off-by: hasbi <muhammadhasbiasidiki175@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+})->name('forgot-password');
