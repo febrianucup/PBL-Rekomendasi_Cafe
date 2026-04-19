df1a38aae5dfe8566d826068ad82968db3483416
Yahya1805 <belajarkuliah902@gmail.com>
2026-04-19 11:52:16 +0700

    Kode buat dasboard owner

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,7 @@ Route::get('/detail/1', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/dashboard', function () {
+    return view('Owner.Dashboard');
+});
