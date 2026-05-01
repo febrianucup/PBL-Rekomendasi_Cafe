2cadca3b6f7515d7f898604edcf07ca229b52d09
febrian <muhammaddwifebrian@gmail.com>
2026-05-01 18:56:33 +0700

    database cafe

---
+++ database/migrations/2026_04_30_170727_create_cafe_photos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cafe_photos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cafe_id')->references('id')->on('cafes');
+            $table->text('photo_url')->isNotEmpty();
+            $table->boolean('is_primary')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cafe_photos');
+    }
+};
