d04b1f88f19f38e6a477fb1b69544f02854af3ae
Muhammad Dwi Febrian <muhammaddwifebrian@google.com>
2026-05-29 00:57:55 +0700

    fix: page load sticky

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -404,5 +404,19 @@
 
     @stack('scripts')
     @livewireScripts
+
+    <script>
+         window.addEventListener('beforeunload', () => {
+            sessionStorage.setItem('scrollPosition', window.scrollY);
+        });
+
+        window.addEventListener('load', () => {
+            const scrollPosition = sessionStorage.getItem('scrollPosition');
+            if (scrollPosition) {
+                window.scrollTo(0, parseInt(scrollPosition));
+                sessionStorage.removeItem('scrollPosition');
+            }
+        });
+    </script>
 </body>
 </html>
